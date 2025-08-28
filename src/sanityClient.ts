fc87b1e72ce814762aae244fa051249b04b005e1
Eryk <erykkokoczkaa@gmail.com>
2025-08-28 22:40:45 +0200

    Ulepszenie klienta Sanity dla Vercel

--- src/sanityClient.ts
+++ src/sanityClient.ts
@@ -3,7 +3,7 @@
 import { createClient } from '@sanity/client';
 
 export default createClient({
-  projectId: 'msnddb47', // Wklej tutaj swoje projectId
+  projectId:  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || 'msnddb47',
   dataset: 'production',
   apiVersion: '2024-08-16', // Użyj aktualnej daty w formacie YYYY-MM-DD
   useCdn: true, // `false` jeśli chcesz zawsze najświeższe dane, `true` dla lepszej wydajności

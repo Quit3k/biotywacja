c6afcdd55a446a0eead20e5d64eb12256cf05894
Eryk <erykkokoczkaa@gmail.com>
2025-11-16 21:07:09 +0100

    Add privacy policy, cookies policy and placeholder regulations

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -11,9 +11,12 @@ const Layout = ({ children }: LayoutProps) => {
   return (
     <div className="min-h-screen flex flex-col">
       <Navbar />
-      <main className="flex-grow">
+
+      {/* GLOBALNY KONTENER PODSTRON */}
+      <main className="flex-grow pt-24 md:pt-28 max-w-4xl mx-auto w-full px-4">
         {children}
       </main>
+
       <Footer />
     </div>
   );
